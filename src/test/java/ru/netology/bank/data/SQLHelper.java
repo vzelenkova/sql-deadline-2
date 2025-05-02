bedeaf4719fedf278b580b9e52679b710f3fc054
vzelenkova <95612348+vzelenkova@users.noreply.github.com>
2025-05-02 09:24:14 +0300

    Update SQLHelper.java

--- src/test/java/ru/netology/bank/data/SQLHelper.java
+++ src/test/java/ru/netology/bank/data/SQLHelper.java
@@ -37,15 +37,4 @@ public class SQLHelper {
         runner.execute(connection, "DELETE FROM cards");
         runner.execute(connection, "DELETE FROM users");
     }
-    @SneakyThrows
-    public static void fillDatabase() {
-        var connection = getConn();
-        runner.execute(connection, "INSERT INTO test_db.users (id,login,password,status) VALUES\n" +
-                "\t ('4271ff5d-4982-4169-8ab1-3e670f0e88e6','vasya','$2a$10$knL6Dw619udjKbjynB2zjO35vqJHMXineuU.biX34td/bHaD1FNnW','active'),\n" +
-                "\t ('c599249d-5a5e-438d-bbd0-ff8e40301d3d','petya','$2a$10$vDUJNyZLaEAtVRgkm2lFq.3rs6B2olULPeIQUHIfugw56Hz5StKi2','active');\n");
-        runner.execute(connection, "INSERT INTO test_db.cards (id,user_id,`number`,balance_in_kopecks) VALUES\n" +
-                "\t ('0f3f5c2a-249e-4c3d-8287-09f7a039391d','4271ff5d-4982-4169-8ab1-3e670f0e88e6','5559 0000 0000 0002',1000000),\n" +
-                "\t ('92df3f1c-a033-48e6-8390-206f6b1f56c0','4271ff5d-4982-4169-8ab1-3e670f0e88e6','5559 0000 0000 0001',1000000);\n");
-    }
-
 }
